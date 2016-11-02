ac2fb2a6006d9a2e0ab7fe2e92d79af58985efd6
Garrett Smith <g@rre.tt>
2016-11-02 01:30:34 +0000

    Hiding errors on prepare op as well

--- src/guild_prepare_cmd.erl
+++ src/guild_prepare_cmd.erl
@@ -32,7 +32,8 @@ parser() ->
       [{pos_args, {0, 1}}]).
 
 prepare_options() ->
-    [{preview, "--preview", "print training details but do not train", [flag]}].
+    [{preview, "--preview", "print training details but do not train", [flag]},
+     {debug, "--debug", "show errors", [hidden, flag]}].
 
 %% ===================================================================
 %% Main
@@ -49,7 +50,7 @@ init_op(Opts, Args) ->
 
 prepare_or_preview({ok, Op}, Opts) ->
     case proplists:get_bool(preview, Opts) of
-        false -> prepare(Op);
+        false -> prepare(Op, Opts);
         true  -> preview(Op)
     end;
 prepare_or_preview({error, Err}, _Opts) ->
@@ -62,8 +63,11 @@ init_op_error(preparable) ->
 init_op_error(Err) ->
     guild_cli:cli_error(guild_cmd_support:runtime_error_msg(Err)).
 
-prepare(Op) ->
+prepare(Op, Opts) ->
+    guild_cmd_support:init_error_tty(debug_flag(Opts)),
     guild_cmd_support:exec_operation(guild_prepare_op, Op).
+
+debug_flag(Opts) -> proplists:get_bool(debug, Opts).
 
 preview(Op) ->
     guild_cli:out_par("This command will use the settings below.~n~n"),
