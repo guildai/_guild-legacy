f13fce21e55ccea97d306cdfe7e7b6c73a4a1c97
Garrett Smith <g@rre.tt>
2017-04-12 16:04:09 -0500

    Moved serve handling into guild_serve_http
    We're not supporting serve ops via View atm. If we want to
    restore this, we can reuse guild_serve_http (e.g. using alternate
    route handling).

--- src/guild_serve_http.erl
+++ src/guild_serve_http.erl
@@ -32,13 +32,52 @@ maybe_start_server({error, Err}, _Project, _Run, _Port) ->
     {error, {port_init, Err}}.
 
 app("POST", "/run", Project, Run, Env) ->
-    Handler = {guild_view_model_http, handle_model_run, [Project, Run]},
+    Handler = {fun handle_model_run/4, [Project, Run]},
     {recv_body, Handler, Env, [{recv_length, ?max_run_request}]};
 app("GET", "/info", Project, Run, _Env) ->
-    guild_view_model_http:handle_model_info(Project, Run);
+    handle_model_info(Project, Run);
 app("GET", "/stats", Project, Run, _Env) ->
-    guild_view_model_http:handle_model_stats(Project, Run);
+    handle_model_stats(Project, Run);
 app("POST", "/init", Project, Run, _Env) ->
-    guild_view_model_http:handle_model_init(Project, Run);
+    handle_model_init(Project, Run);
 app(_, _, _, _, _) ->
     guild_http:bad_request().
+
+%% ===================================================================
+%% Handlers
+%% ===================================================================
+
+handle_model_run(Project, Run, Body, Env) ->
+    Opts = run_project_model_opts(Env),
+    http_result(
+      guild_tensorflow_port:run_project_model(
+        Project, Run, Body, Opts)).
+
+run_project_model_opts(Env) ->
+    {_, _, Params} = psycho:parsed_request_path(Env),
+    case proplists:is_defined("withstats", Params) of
+        true -> [with_stats];
+        false -> []
+    end.
+
+handle_model_info(Project, Run) ->
+    http_result(guild_tensorflow_port:project_model_info(Project, Run)).
+
+handle_model_stats(Project, Run) ->
+    http_result(guild_tensorflow_port:project_model_stats(Project, Run)).
+
+handle_model_init(Project, Run) ->
+    http_result(guild_tensorflow_port:load_project_model(Project, Run)).
+
+%% ===================================================================
+%% Common
+%% ===================================================================
+
+http_result(ok) ->
+    guild_http:ok_no_content();
+http_result({ok, JSON}) ->
+    guild_http:ok_json(JSON);
+http_result({error, <<"not found">>}) ->
+    guild_http:not_found();
+http_result({error, Err}) ->
+    guild_http:bad_request([Err, "\n"]).
