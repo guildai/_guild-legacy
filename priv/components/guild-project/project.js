f93d892089ae8329dcc549348238dda0827137a4
Garrett Smith <g@rre.tt>
2017-06-12 13:29:27 -0500

    guild-fields implemented

--- priv/components/guild-project/project.js
+++ priv/components/guild-project/project.js
@@ -24,4 +24,30 @@ Guild.Project = new function() {
     this.namedSectionOrDefault = function(project, type, name) {
         return project[type + "\t" + name] || project[type];
     };
+
+    this.orderedNamedSections = function(project, type, name) {
+        var sections = [];
+        project.__meta__.sectionOrder.forEach(function(key) {
+            var keyParts = key.split("\t");
+            if (keyParts[0] == type
+                && (keyParts.length == 2
+                    || keyParts[1] == name)) {
+                sections.push(project[key]);
+            }
+        });
+        return sections;
+    };
+
+    this.applyTemplate = function(name, target, project) {
+        var applied = Object.assign({}, target);
+        var template = project["template" + "\t" + name];
+        if (template) {
+            Object.keys(template).forEach(function(key) {
+                if (target[key] == undefined) {
+                    applied[key] = template[key];
+                }
+            });
+        }
+        return applied;
+    };
 };
