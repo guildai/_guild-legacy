ce74d2f259caadae136d12c6c95e7ed7b5c9f539
Garrett Smith <g@rre.tt>
2017-07-17 09:54:51 -0500

    Wip

--- src/guild_list_evals_cmd.erl
+++ src/guild_list_evals_cmd.erl
@@ -26,20 +26,45 @@ parser() ->
       "[OPTION]... [RUNDIR]",
       "List evaluations for a run in RUNIDR or the latest using --latest-run.\n"
       "\n"
-      "Use 'guild list-runs' to list runs that can be used for RUNDIR.",
-      guild_cmd_support:project_options([latest_run]),
+      "Use 'guild list-runs' to list runs that can be used for RUNDIR.\n"
+      "\n"
+      "To list the evalutions for all runs, use --all-runs.",
+      list_eval_options() ++ guild_cmd_support:project_options([latest_run]),
       [{pos_args, {0, 1}}]).
+
+list_eval_options() ->
+    [{all_runs, "-A, --all-runs", "list evaluations for all runs", [flag]}].
 
 %% ===================================================================
 %% Main
 %% ===================================================================
 
 main(Opts, Args) ->
-    RunDir = guild_cmd_support:run_db_for_args(Opts, Args),
-    print_paths(eval_paths(RunDir)).
+    RunDirs = rundirs_for_args(Opts, Args),
+    print_paths(eval_paths(RunDirs)).
 
-eval_paths(RunDir) ->
-    lists:sort(filelib:wildcard(filename:join(RunDir, "eval-*"))).
+rundirs_for_args(Opts, Args) ->
+    case proplists:get_bool(all_runs, Opts) of
+        true ->
+            all_eval_dirs(Opts);
+        false ->
+            [guild_cmd_support:run_db_for_args(Opts, Args)]
+    end.
+
+all_eval_dirs(Opts) ->
+    Project = guild_cmd_support:project_from_opts(Opts),
+    [guild_run:dir(Run) || Run <- guild_run:runs_for_project(Project)].
+
+eval_paths(RunDirs) ->
+    Paths = lists:foldl(fun eval_paths_acc/2, [], RunDirs),
+    lists:sort(Paths).
+
+eval_paths_acc(RunDir, Acc0) ->
+    lists:sort(
+      lists:foldl(
+        fun(Path, Acc) -> [Path|Acc] end,
+        Acc0,
+        filelib:wildcard(filename:join(RunDir, "eval-*")))).
 
 print_paths(Paths) ->
     lists:foreach(fun(P) -> io:format("~s~n", [P]) end, Paths).
